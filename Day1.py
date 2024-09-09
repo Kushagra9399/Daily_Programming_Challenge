f8e1ec640c7996f6920f42d02d20c2ae1f925253
Kushagra9399 <146541055+Kushagra9399@users.noreply.github.com>
2024-09-09 22:26:18 +0530

    Add files via upload

--- Day1.py
+++ Day1.py
@@ -1,19 +1,26 @@
-"""You are given an unsorted array of integers and a positive integer K. Your task is to find the Kth largest element in the array.
-The Kth largest element is the element that would appear in the Kth position if the array were sorted in descending order.
-You need to implement a function that returns this Kth largest element without explicitly sorting the entire array.
-Example
-arr = [3, 2, 1, 5, 6, 4]
-k = 2
-Output: 5
-"""
-def largest(k,arr):
-    for i in range(len(arr)):
-        for j in range(i,len(arr)):
-            if arr[i]<arr[j]:
-                arr[i],arr[j]=arr[j],arr[i]
-    if k>len(arr):
-        return -1
-    else:
-        return arr[k-1]
-    
-print(largest(2,[3,2,1,5,6,4]))
+'''
+Problem: Sort an Array of 0s, 1s, and 2s
+You are given an array arr consisting only of 0s, 1s, and 2s.
+The task is to sort the array in increasing order in linear time (i.e., O(n)) without using any extra space.
+This means you need to rearrange the array in-place.
+
+Input :
+An integer array arr of size n where each element is either 0, 1, or 2.
+Example : arr = [0, 1, 2, 1, 0, 2, 1, 0]
+
+Output :
+The sorted array, arranged in increasing order of 0s, 1s, and 2s.
+Example: [0, 0, 0, 1, 1, 1, 2, 2]
+'''
+
+def day1_sol(arr):
+    a = arr.count(0)
+    b = arr.count(1)
+    c = arr.count(2)
+    arr = []
+    arr.extend([0]*a)
+    arr.extend([1]*b)
+    arr.extend([2]*c)
+    return arr
+
+print(day1_sol([0, 1, 2, 1, 0, 2, 1, 0]))
